0c240812d87d3d8e0132f7dc8bf37c4cf4b91529
haithemelhadj <haithemelhadj42@gmail.com>
2022-12-25 17:26:57 +0100

    commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int Health=3;
+    [SerializeField] private float Fuel=100f;
+    [SerializeField] private int NuclearPower=0;
+    [SerializeField] private int Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
